36f499a423a90c5d5438acc582685a99a042655e
박지훈 <qkrw6@naver.com>
2023-11-17 20:26:11 +0900

    submit

--- src/pages/create-account.tsx
+++ src/pages/create-account.tsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
 import styles from "/src/styles/Create-account.module.css"
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth } from "../../firebase";
+import { FirebaseError } from "firebase/app";
+import { useRouter } from "next/router";
+
 
 
 export default function CreateAccount () {
+  const router = useRouter()
   const [ isLoading, setLoading] = useState(false);
   const [nickname, setNickname] = useState("");
   const [email, setEmail] = useState("");
@@ -10,12 +16,31 @@ export default function CreateAccount () {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
    
-  const onSubmit = (e:React.FormEvent<HTMLFormElement>)=> {
+  const onSubmit = async (e:React.FormEvent<HTMLFormElement>)=> {
+
     e.preventDefault();
     setError("");
     if( isLoading || name === "" || password === "" || email === "") return;
-
-      
+    try{
+      setLoading(true);
+      const credentials = await createUserWithEmailAndPassword(
+           auth,
+           email,
+           password
+      );
+      console.log(credentials.user);
+      await updateProfile(credentials.user, {
+        displayName: name,
+      });
+      router.push("/");
+    }catch(e){
+      if(e instanceof FirebaseError){
+        setError(e.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  
   }
   return (
     <>
